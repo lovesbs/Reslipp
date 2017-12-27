b49a3bd9b7f7221e5cb4a4d19be75559dbb4c6b2
DESKTOP-BILQ87S\SBS <lovesowon@daum.net>
2017-12-27 13:22:17 +0900

    init

---
+++ src/main/java/com/reslipp/ReSlippApplication.java
@@ -0,0 +1,12 @@
+package com.reslipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReSlippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReSlippApplication.class, args);
+	}
+}
