d51aa686991572000998c2e9a2d291d4232eda1f
DESKTOP-BILQ87S\SBS <lovesowon@daum.net>
2017-12-28 15:35:54 +0900

    사옹자 목록, 리스트 구현

---
+++ src/main/java/com/reslipp/web/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.reslipp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+	@GetMapping("/hello")
+	public String welcome(String name,Model model){
+		System.out.println("name:" + name);
+		model.addAttribute("name", name);
+		return "welcome";
+	}
+}
